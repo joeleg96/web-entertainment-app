2ece149308ccf480c0f40cff7b8bf064fe52f96d
Joel Eguiza <joeleg96@gmail.com>
2026-01-02 11:18:02 -0600

    changed BrowserRouter to HashRouter to work with github static SPA build

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import React from "react";
 import { BookmarkProvider } from "./context/BookmarkContext";
 
-
-const basename =
-  import.meta.env.MODE === "production"
-    ? "/web-entertainment-app"
-    : "/";
-
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <BookmarkProvider>
-        <BrowserRouter basename={basename}>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </BookmarkProvider>
     </React.StrictMode>
 )
